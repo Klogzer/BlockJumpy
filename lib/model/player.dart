ad2f47efc1db4a3265c0e2f22f0e7866e19ddcb5
Stuhlsatz, Milan <milan.stuhlsatz@stud.th-luebeck.de>
2019-05-15 18:54:30 +0200

    fix

--- lib/model/player.dart
+++ lib/model/player.dart
@@ -1,13 +1,21 @@
+import 'package:jumpdx9001deluxe/model/dynamic_object.dart';
 import 'package:jumpdx9001deluxe/model/game_element.dart';
 
-class Player extends GameElement {
+class Player extends GameElement with DynamicObject {
 
-  Player() : super(1, 'Player');
+  Player() : super(1, 'player');
 
+  @override
+  void move() {
+    yPosition = yPosition;
+  }
+
+  //gets changes and sets value;
   void moveLeft() {
     xPosition--;
   }
 
+  //gets changes and sets value;
   void moveRight() {
     xPosition++;
   }
