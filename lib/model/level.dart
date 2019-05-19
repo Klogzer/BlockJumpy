7920fa810e71c7d6521b2b2cde40fe27a2552c96
Stuhlsatz, Milan <milan.stuhlsatz@stud.th-luebeck.de>
2019-05-19 21:50:45 +0200

    some test platforms

--- lib/model/level.dart
+++ lib/model/level.dart
@@ -10,6 +10,15 @@ class Level {
     this._player = new Player(this);
     entities.add(_player);
     entities.add(NormalPlatform(500, 100, 100, 20));
+    entities.add(NormalPlatform(100, 300, 100, 20));
+    entities.add(NormalPlatform(800, 500, 100, 20));
+    entities.add(NormalPlatform(50, 700, 100, 20));
+    entities.add(NormalPlatform(500, 750, 100, 20));
+    entities.add(NormalPlatform(1000, 100, 100, 20));
+    entities.add(NormalPlatform(800, 700, 100, 20));
+    entities.add(NormalPlatform(600, 350, 100, 20));
+    entities.add(NormalPlatform(400, 600, 100, 20));
+    entities.add(NormalPlatform(3500, 400, 100, 20));
   }
 
   Player get player => _player;
