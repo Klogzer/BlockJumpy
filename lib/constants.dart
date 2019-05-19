0f0fc1d516b0f9e68432303b8678156d34847877
Stuhlsatz, Milan <milan.stuhlsatz@stud.th-luebeck.de>
2019-05-19 07:11:54 +0200

    refine physics parameters

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,7 +3,7 @@ const int fps = 60;
 // const for modell tick
 const int tickModel = 60;
 // const for gravity
-const double gravity = 2;
+const double gravity = 5;
 // internal Stage width
 const int StageXDimension = 1000;
 // internal Stage height
