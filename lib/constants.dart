3639a076e9966e1d8293ab7bb386742364d87880
Stuhlsatz, Milan <milan.stuhlsatz@stud.th-luebeck.de>
2019-05-19 05:15:00 +0200

    fix physics

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,8 +3,8 @@ const int fps = 60;
 // const for modell tick
 const int tickModel = 60;
 // const for gravity
-const double gravity = 2;
+const double gravity = 1;
 // internal Stage width
 const int StageXDimension = 1000;
 // internal Stage height
-const int StageYDimension = 2000;
+const int StageYDimension = 900;
