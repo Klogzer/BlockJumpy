ad4d06679c51053c3f3626a2c28011d98a5e8a20
marius-mauritz@gmx.de <marius-mauritz@gmx.de>
2019-05-20 13:28:00 +0200

    hitbox is now used for gamelement

--- lib/model/level.dart
+++ lib/model/level.dart
@@ -14,16 +14,16 @@ class Level {
 
     this._player = new Player(this, nextID++, 500, 100, 50, 50);
     entities.add(_player);
-    entities.add(NormalPlatform(nextID++, 500, 100, 100, 20));
-    entities.add(NormalPlatform(nextID++, 100, 300, 100, 20));
-    entities.add(NormalPlatform(nextID++, 800, 500, 100, 20));
-    entities.add(NormalPlatform(nextID++, 50, 700, 100, 20));
-    entities.add(NormalPlatform(nextID++, 500, 750, 100, 20));
-    entities.add(NormalPlatform(nextID++, 1000, 100, 100, 20));
-    entities.add(NormalPlatform(nextID++, 800, 700, 100, 20));
-    entities.add(NormalPlatform(nextID++, 600, 350, 100, 20));
-    entities.add(NormalPlatform(nextID++, 400, 600, 100, 20));
-    entities.add(NormalPlatform(nextID++, 3500, 400, 100, 20));
+    entities.add(NormalPlatform(nextID++, 500, 100, 50, 20));
+    entities.add(NormalPlatform(nextID++, 100, 300, 50, 20));
+    entities.add(NormalPlatform(nextID++, 800, 500, 50, 20));
+    entities.add(NormalPlatform(nextID++, 50, 700, 50, 20));
+    entities.add(NormalPlatform(nextID++, 500, 750, 50, 20));
+    entities.add(NormalPlatform(nextID++, 1000, 100, 50, 20));
+    entities.add(NormalPlatform(nextID++, 800, 700, 50, 20));
+    entities.add(NormalPlatform(nextID++, 600, 350, 50, 20));
+    entities.add(NormalPlatform(nextID++, 400, 600, 50, 20));
+    entities.add(NormalPlatform(nextID++, 3500, 400, 50, 20));
 
 //    _player = Player(this, nextID++, int.parse(json['player'].first), int.parse(json['player'].last), 50, 50);
 //    entities.add(_player);
