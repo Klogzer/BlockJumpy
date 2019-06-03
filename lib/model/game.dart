8039d3540e944753bf5a312a9038dc45c384527c
Marius Mauritz <marius.mauritz@stud.th-luebeck.de>
2019-06-03 12:28:15 +0200

    Levels, objecgies werden nun nur noch gezeigt sofern sie gesetzt sind.

--- lib/model/game.dart
+++ lib/model/game.dart
@@ -6,7 +6,6 @@ class Game {
   int _secondaryScore = 0;
   int _levelID;
 
-
   Game() {
     levelID = 1;
 
@@ -22,13 +21,8 @@ class Game {
     if (_secondaryScore + level.player.score > highscore) {
       highscore = _secondaryScore + level.player.score;
     }
-    if (level.player.score >= level.targetScore &&
-        level.player.xPosition >= level.targetheight &&
-        level.player.jumps >= level.targetJumps &&
-        level.player.platforms >= level.targetPlatforms) {
-      level.won = true;
-      nextLevel();
-    }
+    level.update();
+    if(level.won) nextLevel();
   }
 
   void nextLevel() {
@@ -37,13 +31,12 @@ class Game {
     level.won = true;
   }
 
-
-  // entrypoint for actionlistener
+// entrypoint for actionlistener
   void acceleratePlayer(double dx, double dy) {
     level.player.accelerate(dx, dy);
   }
 
-  // entrypoint for view
+// entrypoint for view
   get entities => level.entities;
 
   int get levelID => _levelID;
@@ -69,6 +62,4 @@ class Game {
   set secondaryScore(int value) {
     _secondaryScore = value;
   }
-
-
 }
--- lib/model/level.dart
+++ lib/model/level.dart
@@ -30,25 +30,22 @@ class Level {
   readLevel(String s) {}
 
   Level.fromJson(json) {
-
     targetScore = json['winCondition'][0];
     targetJumps = json['winCondition'][1];
     targetheight = json['winCondition'][2];
     targetPlatforms = json['winCondition'][3];
     height = json['levelMeta'][0];
-    player = Player(
-        this,
-        nextID++,
-        json['player']["xPos"],
-        json['player']["yPos"],
-        json['player']["lifes"]);
+    player = Player(this, nextID++, json['player']["xPos"],
+        json['player']["yPos"], json['player']["lifes"]);
     entities.add(player);
-    json['normalPlatform'].forEach((json) => entities.add(NormalPlatform.fromJson(nextID++,json)));
-    json['boostPlatform'].forEach((json) =>  entities.add(BoostPlatform.fromJson(nextID++,json)));
-    json['deadlyPlatform'].forEach((json) =>
-        entities.add(DeadlyPlatform.fromJson(nextID++, json)));
-    json['protectionPlatform'].forEach((json) =>
-        entities.add(ProtectionPlatform.fromJson(nextID++, json)));
+    json['normalPlatform'].forEach(
+        (json) => entities.add(NormalPlatform.fromJson(nextID++, json)));
+    json['boostPlatform'].forEach(
+        (json) => entities.add(BoostPlatform.fromJson(nextID++, json)));
+    json['deadlyPlatform'].forEach(
+        (json) => entities.add(DeadlyPlatform.fromJson(nextID++, json)));
+    json['protectionPlatform'].forEach(
+        (json) => entities.add(ProtectionPlatform.fromJson(nextID++, json)));
   }
 
   Map<String, int> getWinCondition() {
@@ -114,4 +111,14 @@ class Level {
     _won = value;
   }
 
+  void update() {
+    if (player.score >= targetScore &&
+        player.xPosition >= targetheight &&
+        player.jumps >= targetJumps &&
+        player.platforms >= targetPlatforms) {
+
+      won = true;
+      print(won);
+    }
+  }
 }
