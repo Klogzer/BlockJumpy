4a1b24fa99f99771eefb8e53bdb4cbc3f6bba541
Marius Mauritz <marius.mauritz@stud.th-luebeck.de>
2019-06-03 13:25:48 +0200

    lowered everything accoridng to 30fps

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,13 +1,13 @@
 // const for view Refreshrate
-const int fps = 60;
+const int fps = 30;
 // const for modell tick
-const int tickModel = 60;
+const int tickModel = 30;
 // const for gravity
-const double gravity = 0.02;
+const double gravity = 0.04;
 // standard jump
-const double stdJump = 0.05;
+const double stdJump = 0.08 ;
 // horizontal accelaration speed
-const double horizontalAccelarationFactor = 0.03;
+const double horizontalAccelarationFactor = 0.05;
 // ...
 const double scrollThreshold = 0.5;
 //points per unvisited platform
